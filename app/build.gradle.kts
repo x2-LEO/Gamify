8e5732d4fa9a3fed275cb59d1abb6a02a969cacc
LazarusCat59 <misturket@tutanota.com>
2023-12-06 22:09:40 +0530

    Edit dependencies
    Added matrix-communication-client and removed matrix-android-sdk2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.5")
     implementation("androidx.navigation:navigation-ui:2.7.5")
-    implementation("org.matrix.android:matrix-android-sdk2:1.2.1")
+    implementation("com.cosium.matrix_communication_client:matrix-communication-client:1.9")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
