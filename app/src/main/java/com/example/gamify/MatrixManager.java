68700c1738dcf20118f89a1d585689a58e4f0546
LazarusCat59 <misturket@tutanota.com>
2023-12-15 19:00:38 +0530

    Example stuff

--- app/src/main/java/com/example/gamify/MatrixManager.java
+++ app/src/main/java/com/example/gamify/MatrixManager.java
@@ -1,17 +1,45 @@
 package com.example.gamify;
 
+import com.cosium.matrix_communication_client.ClientEvent;
 import com.cosium.matrix_communication_client.MatrixResources;
+import com.cosium.matrix_communication_client.RoomResource;
+import com.cosium.matrix_communication_client.Message;
+
+import java.util.List;
 
 public class MatrixManager
 {
+    MatrixResources matrix;
+    RoomResource current_room;
+
     public MatrixManager()
     {
-        MatrixResources matrix = MatrixResources.factory()
+        matrix = MatrixResources.factory()
                 .builder()
                 .https()
-                .hostname("matrix.example.org")
+                .hostname("matrix.org")
                 .defaultPort()
                 .usernamePassword("user", "secret")
                 .build();
+    }
+
+    public void getRoom(String id) {
+        current_room = matrix.rooms().byId(id);
+    }
+
+    public void sendMessage(String message) {
+        Message msgObject = Message.builder().body("hello").build();
+
+        current_room.sendMessage(msgObject);
+    }
+
+    public List<ClientEvent> getClientEventChunk()
+    {
+        return current_room.fetchEventPage("b", "", 10000L, "").chunk();
+    }
+
+    public List<ClientEvent> getClientEventState()
+    {
+        return current_room.fetchEventPage("b", "", 10000L, "").state();
     }
 }
